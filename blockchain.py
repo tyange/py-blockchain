c48ef403b2666145a7fc90f8c9a6f98027e88232
tyange <usun16@gmail.com>
2022-12-01 21:31:15 +0900

    use function

--- blockchain.py
+++ blockchain.py
@@ -1,9 +1,17 @@
-blockchain = [[1]]
+blockchain = []
 
 
-def add_value(transaction_amount):
-    blockchain.append([blockchain[-1], transaction_amount])
+def get_last_blockchain_value():
+    return blockchain[-1]
+
+
+def add_value(transaction_amount, last_transaction=[1]):
+    blockchain.append([last_transaction, transaction_amount])
     print(blockchain)
 
 
 add_value(2)
+add_value(0.9, get_last_blockchain_value())
+add_value(10.89, get_last_blockchain_value())
+
+print(blockchain)
