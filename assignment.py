272ba592f3a29917fbde6721f4b5cbb9de40f120
tyange <usun16@gmail.com>
2022-12-01 22:20:14 +0900

    solved assignment

---
+++ assignment.py
@@ -0,0 +1,18 @@
+name = 'Tyange'
+age = '33'
+
+print(name + age)
+
+
+def print_name_and_age():
+    print('Hi my name is' + name + '. ' + 'Age ' + age + '.')
+
+
+print_name_and_age()
+
+
+def how_much_live():
+    print(int(int(age) / 10))
+
+
+how_much_live()
